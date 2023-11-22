4da0e2fbef8ac74c473fda157610a6b9c0d6ceeb
SukritJBaruah <sukritjyotibaruah@gmail.com>
2023-11-22 23:29:47 +0530

    Rest client interface

---
+++ IRestClient.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Simple.RestClient
+{
+    public interface IRestClient
+    {
+        public Task<Response> GetAsync(Request Request, CancellationToken ct = default);
+        public Task<Response> PostAsync(BodyRequest Request, CancellationToken ct = default);
+        public Task<Response> PatchAsync(BodyRequest Request, CancellationToken ct = default);
+        public Task<Response> PutAsync(BodyRequest Request, CancellationToken ct = default);
+        public Task<Response> DeleteAsync(Request Request, CancellationToken ct = default);
+    }
+}
